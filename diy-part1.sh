2932710f95088517e026ea923485f72b6c1df7b7
codercai <545347837@qq.com>
2021-10-10 19:03:50 +0800

    增加存储相关的能力支持

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,4 +22,3 @@ git clone https://github.com/codercaizh/luci-app-smartdns package/luci-app-smart
 git clone https://github.com/kiddin9/luci-app-eqos package/luci-app-eqos
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
