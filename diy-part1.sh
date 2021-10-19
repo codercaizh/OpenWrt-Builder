46854f7e9992ce34cbcdc9ce8a1b8548341c51d9
codercai <545347837@qq.com>
2021-10-19 18:56:43 +0800

    添加gitclone

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,7 @@ git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 git clone https://github.com/codercaizh/luci-app-smartdns package/luci-app-smartdns
 git clone https://github.com/MapesxGM/luci-app-eqos package/luci-app-eqos
 git clone https://github.com/nickilchen/luci-app-socat package/luci-app-socat
-# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-# git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-# git clone https://github.com/project-lede/luci-app-godproxy package/luci-app-godproxy
-# git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/project-lede/luci-app-godproxy package/luci-app-godproxy
+git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
