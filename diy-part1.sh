f56f9fccba3ded2dc6dd4ffe4da53935881e8e3d
codercai <545347837@qq.com>
2021-10-24 16:12:46 +0000

    r3g 增加chinadns

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,7 @@ git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 git clone https://github.com/codercaizh/luci-app-smartdns package/luci-app-smartdns
 git clone https://github.com/MapesxGM/luci-app-eqos package/luci-app-eqos
 git clone https://github.com/nickilchen/luci-app-socat package/luci-app-socat
+git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 # git clone https://github.com/project-lede/luci-app-godproxy package/luci-app-godproxy
